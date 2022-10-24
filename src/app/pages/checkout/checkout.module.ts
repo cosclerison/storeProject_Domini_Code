a8a1638168ea7c972d0406d7d228287fd1737801
Clerison <cosclerison@gmail.com>
2022-10-23 22:07:16 -0300

    tela details

--- src/app/pages/checkout/checkout.module.ts
+++ src/app/pages/checkout/checkout.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { CheckoutComponent } from './checkout.component';
 import { MaterialModule } from 'src/app/material.module';
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
   declarations: [
-    CheckoutComponent
+    CheckoutComponent,
+    DetailsComponent
   ],
   imports: [
     CheckoutRoutingModule,
