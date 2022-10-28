eead8d90326da91549af461909702d6bb28a7468
Clerison <cosclerison@gmail.com>
2022-10-28 12:30:10 -0300

    carregando e gravando dados Checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,7 +1,12 @@
+import { Router } from '@angular/router';
+import { ShoppingCartService } from './../../shared/services/shopping-cart.service';
+import { Details, Order } from '../products/interfaces/order.interface';
 import { DataService } from './../../shared/services/data.service';
 import { Store } from './../products/interfaces/store.interface';
+import { Product } from '../products/interfaces/product.interface';
 import { Component, OnInit } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
@@ -16,31 +21,78 @@ export class CheckoutComponent implements OnInit {
     city: ''
   };
 
-  isDelivery = false;
-
-  stores: Store[] = []
+  isDelivery = true;
+  cart: Product[] = [];
+  stores: Store[] = [];
 
   constructor(
-    private dataService: DataService
+    private shoppingCartService: ShoppingCartService,
+    private dataService: DataService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
     this.getStores();
+    this.getDataCart();
+    this.prepareDetails();
   }
 
   onPickupOrderDelivery(value:boolean): void {
     this.isDelivery = value;
   }
 
-  onSubmit() {
-    console.log('Caixa')
+  onSubmit({ value: formData }: NgForm): void {
+    console.log('Caixa', formData);
+    const data: Order = {
+      ... formData,
+      date: this.getCurrentDay(),
+      pickup: this.isDelivery
+    }
+    this.dataService.saveOrder(data)
+    .pipe(
+      tap(res => console.log('Order --> 1', res)),
+      switchMap(({  id: orderId }) => {
+        const details = this.prepareDetails();
+        return this.dataService.saveDetailsOrder({ details, orderId });
+      }),
+      tap(() => this.router.navigate(['/thank-you-page'])),
+    )
+    .subscribe();
   }
 
-  private getStores(): void{
+  private getStores(): void{  
     this.dataService
     .getStores()
     .pipe(
       tap((stores:Store[]) => this.stores = stores)
+    )
+    .subscribe();
+  }
+
+  private getCurrentDay(): string {
+    return new Date().toLocaleDateString();
+  }
+
+  private prepareDetails(): Details[]{
+    const details: Details[] = [];
+    this.cart.forEach((product: Product) => {
+      const { 
+        id:productId, 
+        name: productName, 
+        qty: quantity, 
+        stock
+      } = product;
+      details.push( {productId, productName, quantity })
+      
+    })
+    return details;
+
+  }
+
+  private getDataCart(): void {
+    this.shoppingCartService.cartAction$
+    .pipe(
+      tap((products: Product[]) => this.cart = products)
     )
     .subscribe();
   }
